ffd443db1b1154db08ca9b3521917463b6bda070
Ahmed Sermani <33938701+Ahmed-Sermani@users.noreply.github.com>
2020-09-21 11:25:31 +0300

    Update settings.py

--- hrms/settings.py
+++ hrms/settings.py
@@ -5,10 +5,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'enewb*o(nfm0j0!ph6o3nweoz!opa6ly$4=j^&%(-2uquyg1e&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
