4c8093bc1646fef2ba73a876ab912af69352b6be
Ahmad <ahmadsermani58@gmail.com>
2020-12-23 11:00:28 +0300

    Untracked files issue resolved to fix .gitignore

--- core/static/core/js/settingBar.js
+++
@@ -1,7 +0,0 @@
-// Code goes here
-'use strict';
-
-angular.module('myBoot', ['ui.bootstrap'])
-    .controller('NavbarController', ['$scope', function($scope){
-        $scope.isCollapsed = true;
-    }]);
--- core/static/core/js/settingTableStuff.js
+++
@@ -1,3 +0,0 @@
-$("body").on('click-row.bs.table', function (e, row, $element) {
-    window.location = $element.data('href');
-});
