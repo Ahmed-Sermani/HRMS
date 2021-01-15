090f87e1b947c8fbf3b65224b385888d564f6b55
Ahmad <ahmadsermani58@gmail.com>
2021-01-16 01:13:03 +0300

    _

--- employer_dashboard/urls.py
+++ employer_dashboard/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from .views.dashboard import serve_dashboard
 from .views.dashboard import optain_token
+from .views.dashboard import EmployeeAddUpdateView
 app_name = 'employer_dashboard'
 
 urlpatterns = [
     path('' , serve_dashboard , name='employer_dashboard'),
-    path('api/get_token', optain_token, name= 'optain_token')
+    path('api/get_token', optain_token, name= 'optain_token'),
+    path('api/add_update_employee', EmployeeAddUpdateView.as_view(), name='add_update_employee')
 ]
