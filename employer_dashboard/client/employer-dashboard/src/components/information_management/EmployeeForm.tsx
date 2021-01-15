6acf77cc1c310ac59246e8723a0393f01dbfa1b0
Ahmad <ahmadsermani58@gmail.com>
2021-01-16 01:12:52 +0300

    complete the employee add form

--- employer_dashboard/client/employer-dashboard/src/components/information_management/EmployeeForm.tsx
+++ employer_dashboard/client/employer-dashboard/src/components/information_management/EmployeeForm.tsx
@@ -1,54 +1,16 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
 import {
     Form,
     Input,
-    Tooltip,
-    Cascader,
     Select,
-    Row,
-    Col,
-    Checkbox,
     Button,
-    AutoComplete,
-    DatePicker
+    DatePicker,
+    notification
 } from 'antd';
-import { QuestionCircleOutlined } from '@ant-design/icons';
+import { tokenContext } from "../../context";
 const { Option } = Select;
 
-const residences = [
-    {
-        value: 'zhejiang',
-        label: 'Zhejiang',
-        children: [
-            {
-                value: 'hangzhou',
-                label: 'Hangzhou',
-                children: [
-                    {
-                        value: 'xihu',
-                        label: 'West Lake',
-                    },
-                ],
-            },
-        ],
-    },
-    {
-        value: 'jiangsu',
-        label: 'Jiangsu',
-        children: [
-            {
-                value: 'nanjing',
-                label: 'Nanjing',
-                children: [
-                    {
-                        value: 'zhonghuamen',
-                        label: 'Zhong Hua Men',
-                    },
-                ],
-            },
-        ],
-    },
-];
+
 
 const formItemLayout = {
     labelCol: {
@@ -86,46 +48,97 @@ const tailFormItemLayout = {
 
 const EmployeeFrom = () => {
     const [form] = Form.useForm();
+    const tokens = useContext(tokenContext)
 
-    const onFinish = (values: any) => {
-        console.log('Received values of form: ', values);
+    const onFinish = async (values: any) => {
+
+        let parsed_data = { ...values }
+        
+        
+        
+    
+        parsed_data.GOSI_salary = Number(parsed_data.GOSI_salary)
+        parsed_data.base_salary = Number(parsed_data.base_salary)
+        if ( Number.isNaN(parsed_data.GOSI_salary) || Number.isNaN(parsed_data.base_salary) ) {
+            notification.open({
+                type: 'error',
+                message: 'one of the salary fields in not a number'
+            })
+            return
+        }
+    
+        
+        parsed_data.date_of_birth = parsed_data.date_of_birth.format('YYYY-MM-DD')
+        parsed_data.end_of_probation = parsed_data.end_of_probation.format('YYYY-MM-DD')
+        parsed_data.hiring_date = parsed_data.hiring_date.format('YYYY-MM-DD')
+        parsed_data.end_of_contract = parsed_data.end_of_contract.format('YYYY-MM-DD')
+        parsed_data.phone_number = parsed_data.prefix + parsed_data.phone_number
+        delete parsed_data.prefix
+        
+        console.log(parsed_data);
+        
+        const res  = await fetch(process.env.REACT_APP_API + '/add_update_employee', {
+            method: 'POST',
+            body: JSON.stringify(parsed_data),
+            headers: {
+                Authorization: 'Bearer ' + tokens?.access,
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+
+        })
+
+        const result = await res.json()
+
+        if (result.hasOwnProperty('success') && result.success){
+            notification.open({
+                type:'success',
+                message: 'success',
+                description: 'The Employee Has Been Added Successfully'
+            })
+            form.resetFields()
+        }
+        else {
+            notification.open({
+                type:'error',
+                message: 'Failed',
+                description: result.message
+            })
+        }
+
+
+
+
+
+
+
+
+
     };
 
     const prefixSelector = (
         <Form.Item name="prefix" noStyle>
             <Select style={{ width: 70 }}>
-                <Option value="86">+86</Option>
-                <Option value="87">+87</Option>
+                <Option value="966">+966</Option>
+                <Option value="971">+971</Option>
+                <Option value="20">+20</Option>
+                <Option value="963">+963</Option>
             </Select>
         </Form.Item>
     );
 
-    const [autoCompleteResult, setAutoCompleteResult] = useState<string[]>([]);
 
-    const onWebsiteChange = (value: string) => {
-        if (!value) {
-            setAutoCompleteResult([]);
-        } else {
-            setAutoCompleteResult(['.com', '.org', '.net'].map(domain => `${value}${domain}`));
-        }
-    };
 
-    const websiteOptions = autoCompleteResult.map(website => ({
-        label: website,
-        value: website,
-    }));
+
 
     return (
         <Form
             {...formItemLayout}
             form={form}
-            name="register"
+            name="add_employee"
             onFinish={onFinish}
-            initialValues={{
-                residence: ['zhejiang', 'hangzhou', 'xihu'],
-                prefix: '86',
-            }}
             scrollToFirstError
+            size={'large'}
         >
             <Form.Item
                 name="email"
@@ -170,11 +183,41 @@ const EmployeeFrom = () => {
                 <Input />
             </Form.Item>
 
-            <Form.Item 
-                name="date_joined" 
-                label="Date Joined"
+            <Form.Item
+                name="hiring_date"
+                label="Hiring Date"
                 rules={[
                     { type: 'object' as const, required: true, message: 'Please select Date of Join' }
+                ]}
+            >
+                <DatePicker />
+            </Form.Item>
+
+            <Form.Item
+                name="date_of_birth"
+                label="Date Of Birth"
+                rules={[
+                    { type: 'object' as const, required: true, message: 'Please select Date Of Birth' }
+                ]}
+            >
+                <DatePicker />
+            </Form.Item>
+
+            <Form.Item
+                name="end_of_contract"
+                label="End Of Contract"
+                rules={[
+                    { type: 'object' as const, required: true, message: 'Please select End Of Contract' }
+                ]}
+            >
+                <DatePicker />
+            </Form.Item>
+
+            <Form.Item
+                name="end_of_probation"
+                label="End of probation"
+                rules={[
+                    { type: 'object' as const, required: true, message: 'Please select End of probation' }
                 ]}
             >
                 <DatePicker />
@@ -193,21 +236,18 @@ const EmployeeFrom = () => {
                 <Input />
             </Form.Item>
 
+
             <Form.Item
                 name="phone_number"
                 label="Phone Number"
                 rules={[
                     {
                         required: true,
-                        message: 'Please input the Phone Number',
-                    },
-                    {
-                        type: 'number',
-                        message: 'The Phone Number has to a number !?'
+                        message: 'Please input your phone number!'
                     }
                 ]}
             >
-                <Input />
+                <Input addonBefore={prefixSelector} style={{ width: '100%' }} />
             </Form.Item>
 
 
@@ -218,10 +258,6 @@ const EmployeeFrom = () => {
                     {
                         required: true,
                         message: 'Please input the Office Number',
-                    },
-                    {
-                        type: 'number',
-                        message: 'The Office Number has to a number !?'
                     }
                 ]}
             >
@@ -242,127 +278,189 @@ const EmployeeFrom = () => {
                 <Input />
             </Form.Item>
 
-            
 
-            
 
 
             <Form.Item
-                name="password"
-                label="Password"
+                name="national_id"
+                label="National ID"
                 rules={[
                     {
                         required: true,
-                        message: 'Please input your password!',
-                    },
+                        message: 'Please input the National ID',
+                    }
                 ]}
-                hasFeedback
-            >
-                <Input.Password />
-            </Form.Item>
-
-            <Form.Item
-                name="confirm"
-                label="Confirm Password"
-                dependencies={['password']}
-                hasFeedback
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please confirm your password!',
-                    },
-                    ({ getFieldValue }) => ({
-                        validator(_, value) {
-                            if (!value || getFieldValue('password') === value) {
-                                return Promise.resolve();
-                            }
-                            return Promise.reject('The two passwords that you entered do not match!');
-                        },
-                    }),
-                ]}
-            >
-                <Input.Password />
-            </Form.Item>
-
-            <Form.Item
-                name="nickname"
-                label={
-                    <span>
-                        Nickname&nbsp;
-            <Tooltip title="What do you want others to call you?">
-                            <QuestionCircleOutlined />
-                        </Tooltip>
-                    </span>
-                }
-                rules={[{ required: true, message: 'Please input your nickname!', whitespace: true }]}
             >
                 <Input />
             </Form.Item>
 
             <Form.Item
-                name="residence"
-                label="Habitual Residence"
-                rules={[
-                    { type: 'array', required: true, message: 'Please select your habitual residence!' },
-                ]}
-            >
-                <Cascader options={residences} />
-            </Form.Item>
-
-            <Form.Item
-                name="phone"
-                label="Phone Number"
-                rules={[{ required: true, message: 'Please input your phone number!' }]}
-            >
-                <Input addonBefore={prefixSelector} style={{ width: '100%' }} />
-            </Form.Item>
-
-            <Form.Item
-                name="website"
-                label="Website"
-                rules={[{ required: true, message: 'Please input website!' }]}
-            >
-                <AutoComplete options={websiteOptions} onChange={onWebsiteChange} placeholder="website">
-                    <Input />
-                </AutoComplete>
-            </Form.Item>
-
-            <Form.Item label="Captcha" extra="We must make sure that your are a human.">
-                <Row gutter={8}>
-                    <Col span={12}>
-                        <Form.Item
-                            name="captcha"
-                            noStyle
-                            rules={[{ required: true, message: 'Please input the captcha you got!' }]}
-                        >
-                            <Input />
-                        </Form.Item>
-                    </Col>
-                    <Col span={12}>
-                        <Button>Get captcha</Button>
-                    </Col>
-                </Row>
-            </Form.Item>
-
-            <Form.Item
-                name="agreement"
-                valuePropName="checked"
+                name="nationality"
+                label="Nationality"
                 rules={[
                     {
-                        validator: (_, value) =>
-                            value ? Promise.resolve() : Promise.reject('Should accept agreement'),
-                    },
+                        required: true,
+                        message: 'Please input the Nationality',
+                    }
                 ]}
-                {...tailFormItemLayout}
             >
-                <Checkbox>
-                    I have read the <a href="">agreement</a>
-                </Checkbox>
+                <Input />
             </Form.Item>
+
+
+            <Form.Item
+                name="job_title"
+                label="Job Title"
+                rules={[
+                    { required: true, message: 'Please Input The Job Title' },
+                ]}
+            >
+                <Input />
+            </Form.Item>
+
+
+            <Form.Item
+                name="work_type"
+                label="Work Type"
+                rules={[
+                    { required: true, message: 'Please Input The Work Type' },
+                ]}
+            >
+                <Select>
+                    <Select.Option value="Full Time">Full Time</Select.Option>
+                    <Select.Option value="Part Time">Part Time</Select.Option>
+                    <Select.Option value="Remotely">Remotely</Select.Option>
+                </Select>
+            </Form.Item>
+
+
+            <Form.Item
+                name="marital_status"
+                label="Marital Status"
+                rules={[
+                    { required: true, message: 'Please Input The Marital Status' },
+                ]}
+            >
+                <Select>
+                    <Select.Option value="Single">Single</Select.Option>
+                    <Select.Option value="Married">Married</Select.Option>
+                </Select>
+            </Form.Item>
+
+            <Form.Item
+                name="gender"
+                label="Gender"
+                rules={[
+                    { required: true, message: 'Please Input The Gender' },
+                ]}
+            >
+                <Select>
+                    <Select.Option value="Male">Male</Select.Option>
+                    <Select.Option value="Female">Female</Select.Option>
+                </Select>
+            </Form.Item>
+
+            <Form.Item
+                name="salutation"
+                label="Salutation"
+                rules={[
+                    { required: true, message: 'Please Input The Salutation' },
+                ]}
+            >
+                <Select>
+                    <Select.Option value="Prof">Prof</Select.Option>
+                    <Select.Option value="Master">Master</Select.Option>
+                    <Select.Option value="Miss">Miss</Select.Option>
+                    <Select.Option value="Mrs">Mrs</Select.Option>
+                    <Select.Option value="Dr">Dr</Select.Option>
+                    <Select.Option value="Mr">Mr</Select.Option>
+                    <Select.Option value="Ms">Ms</Select.Option>
+                    <Select.Option value="Eng">Eng</Select.Option>
+                    <Select.Option value="Dev">Dev</Select.Option>
+                </Select>
+            </Form.Item>
+
+            <Form.Item
+                name="direct_manager"
+                label="Direct manager"
+                rules={[{
+                    type: 'email',
+                    message: 'The input is not valid E-mail!',
+                },]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                name="supervisor"
+                label="Supervisor"
+                rules={[{
+                    type: 'email',
+                    message: 'The input is not valid E-mail!',
+                }]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                name="section"
+                label="Section"
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                name="work_location"
+                label="Work location"
+                rules={[
+                    { required: true, message: 'Please Input The Work location' },
+                ]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                name="branch"
+                label="Branch"
+                rules={[
+                    { required: true, message: 'Please Input The Branch' },
+                ]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                name="base_salary"
+                label="Base Salary"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input the Base Salary',
+                    }
+                ]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                name="GOSI_salary"
+                label="GOSI Salary"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input the GOSI Salary',
+                    }
+                ]}
+            >
+                <Input />
+            </Form.Item>
+
+
+
             <Form.Item {...tailFormItemLayout}>
                 <Button type="primary" htmlType="submit">
-                    Register
-        </Button>
+                    Add Employee
+                </Button>
             </Form.Item>
         </Form>
     );
--- employer_dashboard/views/dashboard.py
+++ employer_dashboard/views/dashboard.py
@@ -1,14 +1,22 @@
 from django.contrib.auth.decorators import login_required
-from core.decorators import  employer_required
+from core.decorators import employer_required
 from django.http import HttpResponse
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import views
+from rest_framework import permissions
+from rest_framework import decorators
+from rest_framework.response import Response
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.db.utils import IntegrityError
+from django.db import transaction
 
 @login_required
 @employer_required
 def serve_dashboard(request):
     return HttpResponse(b'Employer Dashboard')
+
 
 @csrf_exempt
 @login_required
@@ -18,5 +26,99 @@ def optain_token(request):
     return JsonResponse({
         'refresh': str(refresh),
         'access': str(refresh.access_token),
-        'name': request.user.first_name +' ' + request.user.last_name
+        'name': request.user.first_name + ' ' + request.user.last_name
     })
+
+
+class EmployeeAddUpdateView(views.APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    @method_decorator([employer_required])
+    def post(self, request):
+        from core.models import User, Employee
+        from employee_dashboard.models.Employee_Extra_Info import Employee_Extra_Info
+        from dateutil.parser import parse
+        from django.core.exceptions import ValidationError
+        from employee_dashboard.models.Section import Section
+
+        data = request.data
+        user = User()
+        user.email = data['email']
+        user.first_name = data['first_name']
+        user.last_name = data['last_name']
+        user.date_joined = data['hiring_date']
+        user.username = data['username']
+        user.position = data['position']
+        user.national_id = data['national_id']
+        user.phone_number = data['phone_number']
+        user.office_phone = data['office_number']
+        user.date_of_birth = data['date_of_birth']
+        user.is_employee = True
+
+        
+
+        employee = Employee()
+        employee.employer = request.user.employer
+        employee.user = user
+        
+
+        employee_extra = Employee_Extra_Info()
+
+        employee_extra.employee = employee
+        employee_extra.nationality = data['nationality']
+        employee_extra.job_title = data['job_title']
+
+        try:
+            employee_extra.direct_manager = Employee.objects.get(user__email = data.get('direct_manager')) if  data.get('direct_manager') else None
+            employee_extra.supervisor = Employee.objects.get(user__email = data.get('supervisor')) if  data.get('supervisor') else None
+        except Employee.DoesNotExist:
+            return Response({
+                'success': False,
+                'message': 'Supervisor or Direct Manager Does Not Exist'
+            })
+        
+        try:
+            employee_extra.section = Section.objects.get(name = data.get('section') ) if data.get('section') else None  
+        except Section.DoesNotExist:
+            return Response({
+                'success': False,
+                'message': f'The Section {} Does Not Exist'.format(data['section'])
+            })
+
+        employee_extra.birth_date = data['date_of_birth']
+        employee_extra.hiring_date = data['hiring_date']
+        employee_extra.end_of_contract = data['end_of_contract']
+        employee_extra.end_of_probation = data['end_of_probation']
+        employee_extra.marital_status = data['marital_status']
+        employee_extra.branch = data['branch']
+        employee_extra.sick_day_balance = 30
+        employee_extra.base_salary = data['base_salary']
+        employee_extra.GOSI_salary = data['GOSI_salary']
+        employee_extra.available_annual_vacation_balance = 30
+        employee_extra.gender = data['gender']
+        employee_extra.salutation = data['salutation']
+        employee_extra.status = 'Active'
+
+
+        try:
+            with transaction.atomic():
+                user.save()
+                employee.save()
+                employee_extra.save()
+
+        except ValidationError as e:
+            return Response({
+                'success': False,
+                'message': e.message
+            })
+        except  IntegrityError:
+            return Response({
+                'success': False,
+                'message': 'Email or Username Already Exist'
+            })
+        
+    
+        return Response({'success': True})
+
+
+        
